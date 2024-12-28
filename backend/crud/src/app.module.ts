46bfb9abc8a39369721290f1c37d5c91ab5021eb
Md Salman <99859514+mohdslmn@users.noreply.github.com>
2024-12-28 20:42:05 +0530

    Update app.module.ts

--- backend/crud/src/app.module.ts
+++ backend/crud/src/app.module.ts
@@ -1,17 +1,3 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { CatsModule } from './cats/cats.module';
-// import { MongooseModule } from '@nestjs/mongoose';
-
-// @Module({
-//   imports: [MongooseModule.forRoot("mongodb+srv://mdslmn:hpIm54Cd7MG7uoHJ@nextapi.88dcl.mongodb.net/cat-record-db?retryWrites=true&w=majority&appName=NextAPI"),
-//     CatsModule],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
 // src/app.module.ts
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -20,7 +6,7 @@ import { UploadModule } from './upload/upload.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb+srv://mdslmn:hpIm54Cd7MG7uoHJ@nextapi.88dcl.mongodb.net/?retryWrites=true&w=majority&appName=NextAPI'), // Replace with your MongoDB connection string
+    MongooseModule.forRoot(''), // Replace with your MongoDB connection string
     UploadModule,
   ],
 })
